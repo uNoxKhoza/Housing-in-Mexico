ed9f3306142b688c7800ab93daa3e72cecf1f156
XoloKhoza <noxkhoza88@gmail.com>
2024-11-25 16:25:33 +0200

    cleaned df2

--- data_wrangling_with_pandas.py
+++ data_wrangling_with_pandas.py
@@ -8,23 +8,17 @@ preparation is importing your raw data and cleaning it.
 
 # Load CSV files into DataFrames
 df1 = pd.read_csv("data/mexico-real-estate-1.csv",encoding="ISO-8859-1")
-# df2 = pd.read_csv("data/mexico-real-estate-2.csv" , encoding="ISO-8859-1")
-# df3 = pd.read_csv("data/mexico-real-estate-3.csv", encoding="ISO-8859-1")
+df2 = pd.read_csv("data/mexico-real-estate-2.csv" , encoding="ISO-8859-1")
+df3 = pd.read_csv("data/mexico-real-estate-3.csv", encoding="ISO-8859-1")
 
-# print(df1.head())
-# print(df2.head())
-# print(df3.head())
 
 '''
-Clean df1 by dropping rows with NaN values,Remove column "unnamed". Then remove the "$" 
+Clean df1 by dropping rows with NaN values. Then remove the "$" 
 and "," characters from "price_usd" and recast the values in the 
 column as floats.
 '''
 # Drop rows with any NaN values from df1
 df1.dropna(inplace=True)
-
-# removing unwanted column called  "unamed: 0"
-df1.drop(columns = ["Unnamed: 0"], inplace = True)
 
 # Ensure "price_usd" is treated as a string before cleaning
 df1["price_usd"] = (
@@ -35,5 +29,21 @@ df1["price_usd"] = (
     .astype(float)  # Convert cleaned strings to float
 )
 
-# Print the cleaned dataframe's first 5 rows
-print(df1.head())
+
+'''
+Clean df2
+First, drop rows with NaN values in df2. Next, use the "price_mxn"
+column to create a new column named "price_usd". (Keep in mind 
+that, when this data was collected in 2014, a dollar cost 19 pesos.)
+Finally, drop the "price_mxn" from the DataFrame.
+'''
+# removing unwanted column called  "unamed: 0"
+df2.drop(columns = ["Unnamed: 0"], inplace = True)
+
+#drop all NaN rows
+df2.dropna(inplace= True)
+#create new column "price_used" from price_mxm(mexican pesos) and drop price_mxn column
+#data was collected in 2014, a dollar cost 19 pesos
+df2['price_usd'] = (df2['price_mxn'] / 19).round(2)
+df2.drop(columns = ['price_mxn'], inplace = True)
+
