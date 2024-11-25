d861c42f118772cfe19413178dfca19bdd3f8427
XoloKhoza <noxkhoza88@gmail.com>
2024-11-25 19:05:59 +0200

    plotted mapbox

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -14,3 +14,26 @@ df = pd.read_csv("data/mexico-real-estate-clean.csv")
 print(type(df))
 print(df.shape)
 print(df.head())
+
+'''
+Add "lat" and "lon" to the code below, and run the code. You'll
+see a map that's centered on Mexico City, and you can use the
+"Zoom Out" button in the upper-right corner of the map so that
+you can see the whole country. 
+'''
+# Use plotly express to create figure
+fig = px.scatter_mapbox(
+    df,  # Our DataFrame
+    lat= "lat",
+    lon= "lon",
+    center={"lat": 19.43, "lon": -99.13},  # Map will be centered on Mexico City
+    width=600,  # Width of map
+    height=600,  # Height of map
+    hover_data=["price_usd"],  # Display price when hovering mouse over house
+)
+
+# Add mapbox_style to figure layout
+fig.update_layout(mapbox_style="open-street-map")
+
+# Show figure
+fig.show()
