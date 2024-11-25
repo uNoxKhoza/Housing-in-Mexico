9c5dc1da902e0d92c1204edb7277ecaeccf93670
XoloKhoza <noxkhoza88@gmail.com>
2024-11-25 19:01:05 +0200

    imported data from data wrangling

---
+++ exploratory_data_analysis.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+
+'''
+Read the CSV file that you created in the last notebook 
+("../small-data/mexico-real-estate-clean.csv") into a DataFrame 
+named df
+'''
+# Import "data/mexico-real-estate-clean.csv"
+df = pd.read_csv("data/mexico-real-estate-clean.csv")
+
+# Print object type, shape, and head
+print(type(df))
+print(df.shape)
+print(df.head())
